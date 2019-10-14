910040419b908ea6474bb822ee46d328743c23ef
srbm <shawnrbm@gmail.com>
2019-10-14 12:06:46 -0700

    conditional rendering lesson

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
-import Split from './composition/Split';
+// import Split from './composition/Split';
 import './App.css';
-import Tooltip from './composition/Tooltip';
-import Messages from './Messages';
-import TheDate from './state/TheDate';
-import Counter from './state/Counter';
+// import Tooltip from './composition/Tooltip';
+// import Messages from './Messages';
+// import TheDate from './state/TheDate';
+// import Counter from './state/Counter';
+import Tabs from './state/Tabs';
 
-const firstTooltip = (
-  <Tooltip message='tooltip message' color='hotpink'>ipsum</Tooltip>
-);
-const secondTooltip = (
-  <Tooltip message='another tooltip message' color='#126BCC'>officiis</Tooltip>
-);
-
-function App() {
-  return (
-    <main className='App'>
-      {/* <Split className='left' flexBasis={2}>
-      change to children This is the content for the left `Split`. Lorem {firstTooltip} dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
-      <Tooltip message='one more tooltip message'>Necessitatibus?</Tooltip>
-      </Split>
-      <Split className='right'>
-        This is the content for the right `Split`. Inventore aliquid cupiditate suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo possimus id soluta aspernatur.
-      </Split> */}
-      <TheDate />
-      <Counter count={123} step={0} />
-      <Messages name="Messages" unread={0}/>
-      <Messages name="Notifications" unread={10}/>
-    </main>
-  );
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
+class App extends React.Component {
+  render() {
+    return <div>
+      <Tabs tabs={tabsProp} />
+    </div>
+  }
 }
+
 
 export default App;
---
+++ src/state/Tabs.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import Tabs from './Tabs';
+import { exportAllDeclaration } from '@babel/types';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+describe('Tabs Component', () => {
+  const tabsProp = [
+    { name: 'First tab',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+    { name: 'Second tab',
+      content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+    { name: 'Third tab',
+      content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+  ];
+
+  it('renders without errors', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Tabs />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders empty give no tabs', () => {
+    const wrapper = shallow(<Tabs />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('renders the first tab by default', () => {
+    const tree = renderer.create(<Tabs tabs={tabsProp} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('closes the first tab and opens any clicked tab', () => {
+    const wrapper = shallow(<Tabs tabs={tabsProp} />);
+    console.log('>>> WRAPPER <<<');
+    console.log(wrapper.debug());
+    console.log('>>> FIND(BUTTON) <<<');
+    console.log(wrapper.find('button').at(1).debug());
+    wrapper.find('button').at(1).simulate('click');
+    expect(toJson(wrapper)).toMatchSnapshot();
+  })
+  
+});
